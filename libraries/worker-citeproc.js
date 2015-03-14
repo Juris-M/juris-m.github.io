42b56e084776ddb2d6ea03ed2123cf2ac71be826
Frank Bennett <biercenator@gmail.com>
2015-03-15 03:30:09 +0900

    Cite sampling works

--- libraries/worker-citeproc.js
+++ libraries/worker-citeproc.js
@@ -7,7 +7,7 @@ var selectedVars = {};
 var unselectedVars = {};
 var currentItemType = 'Journal Article';
 
-var sampleData = {};
+var sampleData = null;
 
 var processorElements = {
     style: null,
@@ -24,6 +24,98 @@ Sys.prototype.retrieveLocale = function(lang){
 };
 var sys = new Sys();
 
+function cloneObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+function makeItems() {
+    var fieldBundle = itemTypeData[currentItemType]
+    var item = {
+        id: 'ITEM-1',
+        type: fieldBundle.cslType
+    }
+    for (var key in selectedVars) {
+        if (itemTypeData[currentItemType].creators[key]) {
+            item[key] = sampleData[key](1);
+        } else {
+            item[key] = sampleData[key];
+        }
+    }
+    var baseTitle = '';
+    if (item.title) {
+        baseTitle = item.title;
+        item.title = (baseTitle + '-A');
+    }
+    processorElements.items['ITEM-1'] = cloneObject(item);
+    
+    item = cloneObject(item);
+    item.id = 'ITEM-2';
+    if (item.title) {
+        item.title = (baseTitle + '-B');
+    }
+    processorElements.items['ITEM-2'] = item;
+    
+    item = cloneObject(item);
+    for (var key in selectedVars) {
+        if (itemTypeData[currentItemType].creators[key]) {
+            item[key] = sampleData[key](2);
+        }
+    }
+    if (item.title) {
+        item.title = (baseTitle + '-C');
+    }
+    processorElements.items['ITEM-3'] = item;
+    
+    item = cloneObject(item);
+    for (var key in selectedVars) {
+        if (itemTypeData[currentItemType].creators[key]) {
+            item[key] = sampleData[key](10);
+        }
+    }
+    if (item.title) {
+        item.title = (baseTitle + '-D');
+    }
+    processorElements.items['ITEM-4'] = item;
+}
+
+var CitationFactory = function(){
+    this.citations = [];
+}
+
+CitationFactory.prototype.getBreadCrumbs = function () {
+    var ret = [];
+    for (var i=0,ilen=this.citations.length;i<ilen;i++) {
+        ret.push(["CITATION-"+(i+1),(i+1)]);
+    }
+    return ret;
+}
+
+CitationFactory.prototype.addCitation = function(itemID, label, locator) {
+    var citation = [
+        {
+            "citationID": "CITATION-" + (this.citations.length + 1), 
+            "citationItems": [
+                {
+                    "id": itemID
+                }
+            ], 
+            "properties": {
+                "noteIndex": (this.citations.length + 1)
+            }
+        }, 
+        this.getBreadCrumbs(), 
+        []
+    ]
+    if (label) {
+        citation[0].citationItems[0].label = label;
+    }
+    if (locator) {
+        citation[0].citationItems[0].locator = locator;
+    }
+    this.citations.push(citation);
+}
+
+
 function generateSample() {
     // Samples
     // * One author, plain title
@@ -34,24 +126,26 @@ function generateSample() {
     // * Third ref with two authors
     // * Fourth ref with ten authors
     // * id without locator (and so supra beyond 5)
-    var fieldBundle = itemTypeData[currentItemType]
-    var item = {
-        id: 'Item-1',
-        type: fieldBundle.cslType
+    makeItems();
+    var citationFactory = new CitationFactory();
+    citationFactory.addCitation('ITEM-1', 'page', '11');
+    citationFactory.addCitation('ITEM-2', 'paragraph', '22');
+    citationFactory.addCitation('ITEM-2', 'section', '33');
+    citationFactory.addCitation('ITEM-2', 'section', '33');
+    citationFactory.addCitation('ITEM-1', 'chapter', '44');
+    citationFactory.addCitation('ITEM-3', 'paragraph', '55');
+    citationFactory.addCitation('ITEM-4', 'section', '66');
+    citationFactory.addCitation('ITEM-2');
+    var citationResults = [];
+    var citations = citationFactory.citations;
+    for (var i=0,ilen=citations.length;i<ilen;i++) {
+        res = citeproc.processCitationCluster(citations[i][0], citations[i][1], citations[i][2]);
+        for (var j=0,jlen=res[1].length;j<jlen;j++) {
+            citationResults[res[1][j][0]] = '<li>' + res[1][j][1] + '</li>';
+        }
     }
-    for (var key in selectedVars) {
-        item[key] = JSON.parse(JSON.stringify(sampleData[key]));
-    }
-    processorElement.items['Item-1'] = item;
-    
-    item2 = JSON.parse(JSON.stringify(item));
-    item2.id = 'Item-2';
-    if (item2.title) {
-        item2.title += '-A';
-    }
-    processorElement.items['Item-2'] = item2;
-    
-    item3 = JSON.parse(JSON.stringify(item));
+    var bibliographyResults = citeproc.makeBibliography();
+    return {citations:citationResults,bibliography:bibliographyResults[1]};
 };
 
 function workerExec(func, msg) {
@@ -80,18 +174,29 @@ function getBubbles(event, itemTypeLabel, initVars) {
     var unselected = '';
     var selected = '';
     if (!sampleData) {
-        for (var fieldLabel in fieldBundle.creators) {
-            var cslVarname = fieldBundle.creators[fieldLabel];
-            sampleData[cslVarname] = {
-                family: fieldLabel,
-                given: fieldLabel.slice(0,3) + 'bert'
-            }
+        sampleData = {};
+        var fieldBundle = itemTypeData[currentItemType];
+        for (var cslVarname in fieldBundle.creators) {
+            var fieldLabel = fieldBundle.creators[cslVarname];
+            sampleData[cslVarname] = function(fieldLabel) {
+                var ext = ['ay','bee','cee','dee','ee','ef','gee','aich','aie','jay','kay'];
+                return function(num) {
+                    var ret = [];
+                    for (var i=0,ilen=num;i<ilen;i++) {
+                        ret.push({
+                            family: fieldLabel + ext[i],
+                            given: fieldLabel.slice(0,3) + 'bert'
+                        });
+                    }
+                    return ret;
+                }
+            }(fieldLabel);
         }
         var year = 1950;
         var month = 1;
         var day = 2;
-        for (var fieldLabel in fieldBundle.dateFields) {
-            cslVarname = fieldBundle.dateFields[fieldLabel];
+        for (var cslVarname in fieldBundle.dateFields) {
+            fieldLabel = fieldBundle.dateFields[cslVarname];
             sampleData[cslVarname] = {
                 'date-parts':[
                     [year, month,day]
@@ -101,14 +206,14 @@ function getBubbles(event, itemTypeLabel, initVars) {
             month += 1;
             day += 3;
         }
-        for (var fieldLabel in fieldBundle.numericFields) {
-            cslVarname = fieldBundle.dateFields[fieldLabel];
-            var count = 1;
+        var count = 1;
+        for (var cslVarname in fieldBundle.numericFields) {
+            fieldLabel = fieldBundle.numericFields[cslVarname];
             sampleData[cslVarname] = "" + count + count + count;
             count += 1;
         }
-        for (var fieldLabel in fieldBundle.textFields) {
-            cslVarname = fieldBundle.dateFields[fieldLabel];
+        for (var cslVarname in fieldBundle.textFields) {
+            fieldLabel = fieldBundle.textFields[cslVarname];
             sampleData[cslVarname] = fieldLabel;
         }
     }
@@ -117,8 +222,8 @@ function getBubbles(event, itemTypeLabel, initVars) {
     var segments = ['creators','dateFields','numericFields','textFields'];
     for (var i=0,ilen=segments.length;i<ilen;i++) {
         var segment = segments[i];
-        for (var fieldLabel in fieldBundle[segment]) {
-            var cslVarname = fieldBundle[segment][fieldLabel];
+        for (var cslVarname in fieldBundle[segment]) {
+            var fieldLabel = fieldBundle[segment][cslVarname];
             var defaultUnused = excludeFields[cslVarname] || (cslVarname === 'jurisdiction' && legalTypes.indexOf(itemTypeLabel) === -1);
             var useMe = initVars ? defaultUnused : unselectedVars[cslVarname];
             if (useMe) {
@@ -131,8 +236,8 @@ function getBubbles(event, itemTypeLabel, initVars) {
     // Selected variables
     for (var i=0,ilen=segments.length;i<ilen;i++) {
         var segment = segments[i];
-        for (var fieldLabel in fieldBundle[segment]) {
-            var cslVarname = fieldBundle[segment][fieldLabel];
+        for (var cslVarname in fieldBundle[segment]) {
+            var fieldLabel = fieldBundle[segment][cslVarname];
             var defaultUsed = !excludeFields[cslVarname] && !(cslVarname === 'jurisdiction' && legalTypes.indexOf(itemTypeLabel) === -1);
             var useMe = initVars ? defaultUsed : selectedVars[cslVarname];
             if (useMe) {
@@ -145,6 +250,23 @@ function getBubbles(event, itemTypeLabel, initVars) {
     return [unselected, selected];
 }
 
+function reverseMapping(origObj) {
+    var newObj = {};
+    for (var key in origObj) {
+        newObj[key] = {};
+        newObj[key].cslType = origObj[key].cslType;
+        var segments = ["creators", "dateFields", "numericFields", "textFields"];
+        for (var i=0,ilen=segments.length;i<ilen;i++) {
+            var segment = segments[i];
+            newObj[key][segment] = {};
+            for (var kkey in origObj[key][segment]) {
+                newObj[key][segment][origObj[key][segment][kkey]] = kkey;
+            }
+        }
+    }
+    return newObj;
+}
+
 onmessage = function (event) {
     switch (event.data.type) {
     case 'PING':
@@ -152,13 +274,14 @@ onmessage = function (event) {
         break;
     case 'LOAD STYLE AND SUBMIT LOCALES':
         workerExec(function() {
+            sampleData = null;
             processorElements.style = event.data.style;
-            this.outObj.locales = {};
+            outObj.locales = {};
             for (var locale in event.data.locales) {
                 if (CSL.LANG_BASES[locale]) {
                     locale = CSL.LANG_BASES[locale];
                 }
-                this.outObj.locales[locale] = true;
+                outObj.locales[locale] = true;
             }
         }, 'STYLE OK LOCALES REQUESTED');
         break;
@@ -174,17 +297,11 @@ onmessage = function (event) {
             citeproc = new CSL.Engine(sys, processorElements.style);
         }, 'PROCESSOR OK');
         break;
-    case 'LOAD ITEM':
-        workerExec(function() {
-            processorElement.item = event.data.itemObj;
-            // Figure out locales required for item and include in response
-        }, 'ITEM OK');
-        break;
     case 'INIT PAGE':
         workerExec(function() {
             unselectedVars = {};
             selectedVars = {};
-            itemTypeData = event.data.itemTypeData;
+            itemTypeData = reverseMapping(event.data.itemTypeData);
             excludeFields = event.data.excludeFields;
             legalTypes = event.data.legalTypes;
             outObj.html = '';
@@ -210,18 +327,43 @@ onmessage = function (event) {
             outObj.html += bubbles[1];
             outObj.html += '      </div>'
                 + '    </div>';
+
+            var result = generateSample();
+
             outObj.html += '  </div>\n'
-                + '  <div class="col-lg-6">\n'
+                + '  <div id="sampler-preview" class="col-lg-6">\n'
+                + '    <div class="row"><div class="col-lg-12"><h3>Citations</h3></div></div>'
+                + '    <div class="row">'
+                + '      <div class="col-lg-12">'
+                + '        <ol id="sampler-citations">';
+            for (var i=0,ilen=result.citations.length;i<ilen;i++) {
+                var citation = result.citations[i];
+                outObj.html += citation;
+            }
+            outObj.html += '        </ol>'
+                + '      </div>'
+                + '    </div>'
+                + '    <div class="row"><div class="col-lg-12"><h3>Bibliography</h3></div></div>'
+                + '    <div class="row">'
+                + '      <div id="sampler-bibliography" class="col-lg-12">'
+            for (var i=0,ilen=result.bibliography.length;i<ilen;i++) {
+                var entry = result.bibliography[i];
+                outObj.html += entry;
+            }
+
+            outObj.html +=  '      </div>'
+                + '    </div>'
                 + '  </div>\n'
                 + '</div>\n';
-            //this.outObj.result = generateSample();
         }, 'INIT PAGE OK');
         break;
     case 'CHANGE ITEM TYPE':
         workerExec(function() {
+            sampleData = null;
             unselectedVars = {};
             selectedVars = {};
             outObj.bubbles = getBubbles(event, event.data.itemType, true);
+            outObj.result = generateSample();
         }, 'CHANGE ITEM TYPE OK');
         break;
     case 'SELECT VARIABLE':
@@ -230,6 +372,7 @@ onmessage = function (event) {
             delete unselectedVars[varName];
             selectedVars[varName] = true;
             outObj.bubbles = getBubbles(event);
+            outObj.result = generateSample();
         }, 'SELECT VARIABLE OK');
         break;
     case 'UNSELECT VARIABLE':
@@ -238,6 +381,7 @@ onmessage = function (event) {
             delete selectedVars[varName];
             unselectedVars[varName] = true;
             outObj.bubbles = getBubbles(event);
+            outObj.result = generateSample();
         }, 'UNSELECT VARIABLE OK');
         break;
     }
