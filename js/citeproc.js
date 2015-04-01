9ea1cd1e7bba59b622c5979c3162702c7f3abe7e
Frank Bennett <biercenator@gmail.com>
2015-04-01 19:36:31 +0900

    Tweak

--- js/citeproc.js
+++ js/citeproc.js
@@ -6216,6 +6216,12 @@ CSL.Node.label = {
                     state.parallel.AppendToVariable(item.label);
                     item.section_form_override = this.strings.form;
                 }
+                if (termtxt) {
+                    flag = state.tmp.group_context.value();
+                    //print("setting TERM to true [0]");
+                    flag[0] = true;
+                    state.tmp.group_context.replace(flag);
+                }
                 state.output.append(termtxt, this);
                 if (item && this.strings.term === "locator") {
                     state.parallel.CloseVariable();
