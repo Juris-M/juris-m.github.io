796288fea928b57a0dda706db9cdaccd492e7007
Frank Bennett <biercenator@gmail.com>
2018-02-17 19:27:18 +0900

    Alerts for debugging

--- js/screen.js
+++ js/screen.js
@@ -1,4 +1,7 @@
 function setStartBoxHighlight() {
+    alert('Hello, doing setStartBoxHighlight() OK');
+    alert('Hello, location.search='+location.search);
+    alert('Hello, href='+location.href);
     if (location.search.indexOf('#start') > -1) {
         var browserConnectors = document.getElementById('browser-connectors');
         browserConnectors.classList.add('add-border');
