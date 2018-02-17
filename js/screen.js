f54d29f6472acf839b063432b4777929260beab6
Frank Bennett <biercenator@gmail.com>
2018-02-17 19:20:22 +0900

    Set highlight after load

--- js/screen.js
+++ js/screen.js
@@ -1,4 +1,6 @@
-if (location.search.indexOf('#start') > -1) {
-  var browserConnectors = document.getElementById('browser-connectors');
-  browserConnectors.classList.add('add-border');
+function setStartBoxHighlight() {
+    if (location.search.indexOf('#start') > -1) {
+        var browserConnectors = document.getElementById('browser-connectors');
+        browserConnectors.classList.add('add-border');
+    }
 }
