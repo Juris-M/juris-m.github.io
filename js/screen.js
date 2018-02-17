0df0b9b0d034e123caad05b8c80eedc8f66addf8
Frank Bennett <biercenator@gmail.com>
2018-02-17 19:29:05 +0900

    Use location.href, Luke

--- js/screen.js
+++ js/screen.js
@@ -1,8 +1,5 @@
 function setStartBoxHighlight() {
-    alert('Hello, doing setStartBoxHighlight() OK');
-    alert('Hello, location.search='+location.search);
-    alert('Hello, href='+location.href);
-    if (location.search.indexOf('#start') > -1) {
+    if (location.href.indexOf('#start') > -1) {
         var browserConnectors = document.getElementById('browser-connectors');
         browserConnectors.classList.add('add-border');
     }
